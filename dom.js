51449b96d2413f5c5b2b38fb8a97f55b6fe410f1
Thalles Maia <thalleshmmaia@gmail.com>
2017-06-16 15:42:19 -0300

    First commit

---
+++ dom.js
@@ -0,0 +1,76 @@
+/**
+ * DOM
+ * @param {string} tagName 
+ * @param {object|string} [attributes] 
+ * @param {string|number|HTMLElement|[string|number|HTMLElement]} [children]
+ * @return {HTMLElement}
+ */
+export default function(tagName, attributes, children) {
+
+    if (typeof tagName !== 'string') {
+        throw 'TagName has unexpected type. It expects string.';
+    }
+
+    // Create element
+    const el = document.createElement(tagName);
+
+    if (attributes) {
+        // If attributes is string, 
+        // then attributes is class
+        if (typeof attributes === 'string') {
+            el.className = attributes;
+        
+        // If attributes is object, we
+        // get each property and set as attributes
+        // on the element
+        } else if (typeof attributes === 'object') {
+            const keys = Object.keys(attributes);
+            keys.forEach(key => {
+                const value = attributes[key];
+
+                // If key equals to className
+                // we rename it to key. className is used
+                // because the word 'class' is private in Javascript.
+                if (key === 'className') {
+                    key = 'class';
+                }
+
+                if (typeof value !== 'string') {
+                    throw `Attributes' property '${key}' has unexpected type. It expects string.`;
+                }
+
+                el.setAttribute(key, value);
+            });
+        } else {
+            throw 'Attributes has unexpected type. It expects string or object.';
+        }
+
+    }
+
+    if (children) {
+        // If children is not an array,
+        // we make it an array
+        if (!(children instanceof Array)) {
+            children = [children];
+        }
+
+        children.forEach(child => {
+            
+            // If child is string or number,
+            // we transform it into a text node
+            if (typeof child === 'string' || typeof child === 'number') {
+                child = document.createTextNode(child);
+            
+            // Otherwise, throw error if child is not an HTMLElement
+            } else if (!(child instanceof HTMLElement)) {
+                throw 'Child is not an HTMLElement, nor number nor string';
+            }
+
+            el.appendChild(child);
+
+        });
+
+    }
+
+    return el;
+}
---
+++ readme.md
@@ -0,0 +1,59 @@
+# DOM
+
+This project, much like ReactDOM, helps you handle the DOM inside your javascript code.
+
+## Parameters
+
+|Parameter|Type|Optional|Description|
+|---|---|---|---|
+|tagName|string|no|Element name you are creating|
+|attributes|object/string|yes|If you pass an object, its properties will become the elements attributes. If you pass a string, it will become the elements CSS class.|
+|children|mixed|yes|It can be a string, a number or HTMLElement, or an array of these types. It will be appended to the element.|
+
+## Sample
+
+### 1. Input form
+
+On Javascript
+```JS
+const dom = d('div', 'form-group', [
+    d('label', { 'for':'email' }, 'E-mail:'),
+    d('input', { 'type': 'email', 'id': 'email', 'class': 'form-control' }) 
+]);
+```
+
+Output
+```HTML
+<div class="form-group">
+    <label for="email">E-mail:</label>
+    <input type="email" id="email" class="form-control" />
+</div>
+```
+
+### 2. Menu
+
+On Javascript
+```JS
+const links = [
+    { name:'Home', link: 'home.html' },
+    { name:'About', link: 'about.html' },
+    { name:'Contact', link: 'contact.html' }
+];
+
+const dom = d('ul', 'list-group', links.map(item => {
+    return d('li', 'list-group-item', 
+        d('a', { 'href': item.link }, item.name)
+    );
+}));
+```
+
+Output
+```HTML
+<ul class="list-group">
+    <li class="list-group">
+        <a href="home.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+    </li>
+</ul>
+```
